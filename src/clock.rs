0cc29c13130471d8f8ab1849b64ece86cb8505cd
Joseph Lyons <josephtlyons@gmail.com>
2019-08-22 07:27:24 -0400

    Use `enumerate`

--- src/clock.rs
+++ src/clock.rs
@@ -289,18 +289,18 @@ fn print_time_horizontally (time: time::Tm) {
                          number_eight,
                          number_nine];
 
-    for row in 0..number_zero.len() {
+    for (i, _item) in number_zero.iter().enumerate() {
         print! ("|");
         print! (" ");
-        print! ("{}", clock_numbers[hour_tens_digit][row]);
+        print! ("{}", clock_numbers[hour_tens_digit][i]);
         print! (" ");
-        print! ("{}", clock_numbers[hour_singles_digit][row]);
+        print! ("{}", clock_numbers[hour_singles_digit][i]);
         print! (" ");
-        print! ("{}", number_colon[row]);
+        print! ("{}", number_colon[i]);
         print! (" ");
-        print! ("{}", clock_numbers[min_tens_digit][row]);
+        print! ("{}", clock_numbers[min_tens_digit][i]);
         print! (" ");
-        print! ("{}", clock_numbers[min_singles_digit][row]);
+        print! ("{}", clock_numbers[min_singles_digit][i]);
         print! (" ");
         print! ("|");
         println!();
