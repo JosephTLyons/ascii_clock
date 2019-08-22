2429196b43c60d8793285b83ce029dc4237fe9dc
Joseph Lyons <josephtlyons@gmail.com>
2019-08-22 07:33:54 -0400

    rustfmt on everything but the formatting of the numbers

--- src/clock.rs
+++ src/clock.rs
@@ -1,42 +1,42 @@
 pub fn print_clock() {
     let time = time::now();
     print_horizontal_border();
-    print! ("| ");
-    print_date (time);
-    print_am_or_pm (time);
-    println! (" |");
-    print! ("| ");
+    print!("| ");
+    print_date(time);
+    print_am_or_pm(time);
+    println!(" |");
+    print!("| ");
     print_divider();
-    println! (" |");
-    print_time_horizontally (time);
+    println!(" |");
+    print_time_horizontally(time);
     print_horizontal_border();
 }
 
 fn print_horizontal_border() {
-    println! (" ----------------------------------------------");
+    println!(" ----------------------------------------------");
 }
 
-fn print_date (time: time::Tm) {
-    let weekday = get_day (time.tm_wday);
-    let month = get_month (time.tm_mon);
+fn print_date(time: time::Tm) {
+    let weekday = get_day(time.tm_wday);
+    let month = get_month(time.tm_mon);
     let day_number = time.tm_mday;
     let year = 1900 + time.tm_year;
 
-    print! ("{}: {} {}, {}", weekday, month, day_number, year);
+    print!("{}: {} {}, {}", weekday, month, day_number, year);
 
     // The following code prints out the remaining spaces needed to keep the AM/PM code within
     // the clock border
     let mut spaces_to_print: usize = 32;
 
     spaces_to_print -= weekday.to_string().chars().count()
-                     + month.to_string().chars().count()
-                     + day_number.to_string().chars().count()
-                     + year.to_string().chars().count();
+        + month.to_string().chars().count()
+        + day_number.to_string().chars().count()
+        + year.to_string().chars().count();
 
-    print_space (spaces_to_print);
+    print_space(spaces_to_print);
 }
 
-fn get_day (day_number: i32) -> String {
+fn get_day(day_number: i32) -> String {
     match day_number {
         0 => "Sunday".to_string(),
         1 => "Monday".to_string(),
@@ -44,11 +44,11 @@ fn get_day (day_number: i32) -> String {
         3 => "Wednesday".to_string(),
         4 => "Thursday".to_string(),
         5 => "Friday".to_string(),
-        _ => "Saturday".to_string()
+        _ => "Saturday".to_string(),
     }
 }
 
-fn get_month (month_number: i32) -> String {
+fn get_month(month_number: i32) -> String {
     match month_number {
         0 => "January".to_string(),
         1 => "February".to_string(),
@@ -61,34 +61,32 @@ fn get_month (month_number: i32) -> String {
         8 => "September".to_string(),
         9 => "October".to_string(),
         10 => "November".to_string(),
-        _ => "December".to_string()
+        _ => "December".to_string(),
     }
 }
 
-fn print_space (mut spaces_to_print: usize) {
+fn print_space(mut spaces_to_print: usize) {
     while spaces_to_print > 0 {
-        print! (" ");
+        print!(" ");
         spaces_to_print -= 1;
     }
 }
 
-fn print_am_or_pm (time: time::Tm) {
+fn print_am_or_pm(time: time::Tm) {
     let hour: usize = time.tm_hour as usize;
 
     if hour > 12 {
-        print! (" AM *PM");
-    }
-
-    else {
-        print! ("*AM  PM");
+        print!(" AM *PM");
+    } else {
+        print!("*AM  PM");
     }
 }
 
 fn print_divider() {
-    print! ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 }
 
-fn print_time_horizontally (time: time::Tm) {
+fn print_time_horizontally(time: time::Tm) {
     let hour: usize = time.tm_hour as usize;
     let min: usize = time.tm_min as usize;
     let mut hour_tens_digit: usize = 0;
@@ -99,14 +97,10 @@ fn print_time_horizontally (time: time::Tm) {
     if hour == 0 || hour == 12 {
         hour_tens_digit = 1;
         hour_singles_digit = 2;
-    }
-
-    else if hour > 12 {
+    } else if hour > 12 {
         hour_tens_digit = (hour - 12) / 10;
         hour_singles_digit = (hour - 12) % 10;
-    }
-
-    else {
+    } else {
         hour_singles_digit = hour;
     }
 
@@ -243,19 +237,19 @@ fn print_time_horizontally (time: time::Tm) {
                          number_nine];
 
     for (i, _item) in number_zero.iter().enumerate() {
-        print! ("|");
-        print! (" ");
-        print! ("{}", clock_numbers[hour_tens_digit][i]);
-        print! (" ");
-        print! ("{}", clock_numbers[hour_singles_digit][i]);
-        print! (" ");
-        print! ("{}", number_colon[i]);
-        print! (" ");
-        print! ("{}", clock_numbers[min_tens_digit][i]);
-        print! (" ");
-        print! ("{}", clock_numbers[min_singles_digit][i]);
-        print! (" ");
-        print! ("|");
+        print!("|");
+        print!(" ");
+        print!("{}", clock_numbers[hour_tens_digit][i]);
+        print!(" ");
+        print!("{}", clock_numbers[hour_singles_digit][i]);
+        print!(" ");
+        print!("{}", number_colon[i]);
+        print!(" ");
+        print!("{}", clock_numbers[min_tens_digit][i]);
+        print!(" ");
+        print!("{}", clock_numbers[min_singles_digit][i]);
+        print!(" ");
+        print!("|");
         println!();
     }
 }
